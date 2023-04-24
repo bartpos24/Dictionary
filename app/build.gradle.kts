43f653bd90892c0f7d035f64f801f4f28b705b2b
bartpos24 <bartek.poskart24@gmail.com>
2023-04-24 21:30:06 +0200

    Updated gradle files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,10 +7,16 @@ android {
     namespace = "com.example.dictionary"
     compileSdk = 32
 
+//    signingConfigs {
+//        create("release") {
+//
+//        }
+//    }
+
     defaultConfig {
         applicationId = "com.example.dictionary"
         minSdk = 27
-        targetSdk = 32
+        targetSdk = 31
         versionCode = 1
         versionName = "1.0"
 
@@ -19,7 +25,8 @@ android {
 
     buildTypes {
         getByName("release") {
-            //minifyEnabled = false
+            isMinifyEnabled = false
+            //signingConfig = signingConfigs.getByName("release")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -37,15 +44,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.6.0")
-    implementation("com.google.android.material:material:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.5.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.core:core-ktx:1.6.0-rc01")
+    implementation("androidx.appcompat:appcompat:1.4.0-alpha01")
+    implementation("com.google.android.material:material:1.4.0-rc01")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0-beta02")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.3.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.3.1")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.3.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.3.5")
+    testImplementation("junit:junit:4.13")
+    androidTestImplementation("androidx.test.ext:junit:1.1.2-alpha03")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0-rc01")
 }
